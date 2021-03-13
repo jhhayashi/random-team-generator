a89c9d9f44c676b24cd1d9a4dda71c7281ef8c1c
Jordan Hayashi <jhhayashi@gmail.com>
2021-03-13 18:44:18 -0500

    Extend team generation function to balance all users
    Previously, it could only return teams when both team size and team
    count were passed as args. Now if only one of the above are passed, it
    uses that arg as a constraint and return all users in teams based on
    the constraint

--- server/app.ts
+++ server/app.ts
@@ -14,8 +14,30 @@ if (NODE_ENV == 'test') {
   Object.assign(app, {useFakeData(fakeData: any[]) { team = fakeData }})
 }
 
-// creates `teamCount` teams of up to `maxTeamSize` members
-export function getRandomTeamsFromMembers(users: User[], teamCount: number, maxTeamSize: number) {
+function isPositiveInt(maybeInt: any): boolean {
+  return _.isInteger(maybeInt) && maybeInt > 0
+}
+
+// Record / Object with at least one key defined
+type AtLeastOneKey<O, T = {[K in keyof O]: Pick<O, K>}> = Partial<O> & T[keyof T]
+
+/* creates random teams based on a list of candidate users
+ *
+ * | teamCount | maxTeamSize |
+ * |    x      |     x       | if both are specified, creates `teamCount` teams of up to `maxTeamSize` members
+ * |    x      |     -       | creates `teamCount` teams, distributing all users as evenly as possible
+ * |    -      |     x       | creates [Math.ceil(users / maxTeamSize)] teams and distributes users
+ * |    -      |     -       | errors
+ */
+export function getRandomTeamsFromMembers(
+  users: User[],
+  {teamCount: teamCountArg, maxTeamSize: maxTeamSizeArg}: AtLeastOneKey<Record<'teamCount'|'maxTeamSize',  number>>
+) {
+  if (teamCountArg != undefined && !isPositiveInt(teamCountArg)) throw new Error('teamCount must be a positive integer')
+  if (maxTeamSizeArg != undefined && !isPositiveInt(maxTeamSizeArg)) throw new Error('maxTeamSize must be a positive integer')
+  if (!teamCountArg && !maxTeamSizeArg) throw new Error('At least one of teamCount, maxTeamSize is required')
+  const maxTeamSize = maxTeamSizeArg || users.length
+  const teamCount = teamCountArg || Math.ceil(users.length / maxTeamSize)
   const teams: User[][]  = Array.from({length: teamCount}, _ => ([]))
   const candidates = _.sampleSize(users, teamCount * maxTeamSize)
   candidates.forEach((user, i) => teams[i % teamCount]?.push(user))
@@ -32,7 +54,7 @@ function createResponseFunction<T>() {
 
 const sendAPIV1Member = createResponseFunction<APIv1Member | undefined>()
 app.get('/api/v1/member', (_req: Request, res: Response) => {
-  const randomMember = getRandomTeamsFromMembers(team, 1, 1)[0]?.[0]
+  const randomMember = getRandomTeamsFromMembers(team, {teamCount: 1, maxTeamSize: 1})[0]?.[0]
   sendAPIV1Member(res, randomMember)
 })
 
