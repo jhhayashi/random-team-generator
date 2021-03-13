22a6bd9d8145335ff08e9ec7d4a314ac461b3dab
Jordan Hayashi <jhhayashi@gmail.com>
2021-03-12 19:59:02 -0500

    Add util function for generating teams from a set of users

--- server/app.test.ts
+++ server/app.test.ts
@@ -1,8 +1,9 @@
 import request from 'supertest'
+import * as _ from 'lodash'
 
 import {User} from '../types'
 
-import app from './app'
+import app, {getRandomTeamsFromMembers} from './app'
 
 const data: User[] = [
   {id: "1", name: "One"},
@@ -10,8 +11,33 @@ const data: User[] = [
   {id: "3", name: "Three"},
 ]
 
+describe('Test getRandomTeamsFromMembers()', () => {
+  it('returns the correct number of empty arrays when there are no users', () => {
+    for (let i = 1; i <= 3; i+= 1) {
+      const teams= getRandomTeamsFromMembers([], i, 10)
+      expect(teams).toHaveLength(i)
+      teams.forEach(team => expect(team).toHaveLength(0))
+    }
+  })
+  it('returns disjoint teams', () => {
+    const teams = getRandomTeamsFromMembers(data, data.length, 1)
+    expect(teams).toHaveLength(data.length)
+    const ids = [...new Set(_.flatMap(teams, team => team.map(user => user.id)))]
+    expect(ids).toHaveLength(data.length)
+  })
+  it('returns a team with all members', () => {
+    const teams = getRandomTeamsFromMembers(data, 1, data.length + 5) // 5 here is arbitrary
+    expect(teams).toHaveLength(1)
+    const team = teams[0]
+    expect(team).toHaveLength(data.length)
+    const ids = [...new Set(team.map(user => user.id))]
+    expect(ids).toHaveLength(data.length)
+  })
+})
+
 describe('Test the /api/v1/member endpoint', () => {
   it('Returns a random team member', () => {
+    // @ts-ignore
     app.useFakeData(data)
     return request(app)
       .get('/api/v1/member')
--- server/app.ts
+++ server/app.ts
@@ -14,6 +14,14 @@ if (NODE_ENV == 'test') {
   Object.assign(app, {useFakeData(fakeData: any[]) { team = fakeData }})
 }
 
+// creates `teamCount` teams of up to `maxTeamSize` members
+export function getRandomTeamsFromMembers(users: User[], teamCount: number, maxTeamSize: number) {
+  const teams: User[][]  = Array.from({length: teamCount}, _ => ([]))
+  const candidates = _.sampleSize(users, teamCount * maxTeamSize)
+  candidates.forEach((user, i) => teams[i % teamCount]?.push(user))
+  return teams
+}
+
 // since there's no good way to type an express response, we have to add some
 // small overhead that types the response for us
 function createResponseFunction<T>() {
@@ -24,7 +32,7 @@ function createResponseFunction<T>() {
 
 const sendAPIV1Member = createResponseFunction<APIv1Member | undefined>()
 app.get('/api/v1/member', (_req: Request, res: Response) => {
-  const randomMember = _.sample(team)
+  const randomMember = getRandomTeamsFromMembers(team, 1, 1)[0]?.[0]
   sendAPIV1Member(res, randomMember)
 })
 
