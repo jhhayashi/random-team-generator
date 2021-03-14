2dfdf45b8ae3b4d31c8c019bf93748713096b6f6
Jordan Hayashi <jhhayashi@gmail.com>
2021-03-14 18:39:44 -0400

    Add function for fetching and caching bamboohr employees

---
+++ server/bamboo.ts
@@ -0,0 +1,143 @@
+import * as _ from 'lodash'
+import base64 from 'base-64'
+import fetch from 'node-fetch'
+
+const {
+  BAMBOOHR_KEY,
+  BAMBOOHR_SUBDOMAIN,
+  CACHE_EXPIRATION_MS: CACHE_EXPIRATION_MS_STR = 1000 * 60 * 60 * 24, // 1 day
+} = process.env
+
+const CACHE_EXPIRATION_MS = +CACHE_EXPIRATION_MS_STR
+const ALLOWED_KEYS = ['id', 'displayName', 'preferredName', 'jobTitle', 'department', 'location', 'photoUrl', 'name']
+
+const ENABLED = BAMBOOHR_KEY && BAMBOOHR_SUBDOMAIN
+
+if (!ENABLED) console.log('No BAMBOOHR_KEY and BAMBOOHR_SUBDOMAIN. BambooHR integration turned off')
+
+type ID = string
+
+// https://documentation.bamboohr.com/docs/list-of-field-names
+interface BambooEmployee {
+  id: string | number
+  displayName?: string
+  preferredName?: string
+  jobTitle?: string | string[]
+  department?: string | string[]
+  location?: string | string[]
+  photoUrl?: string
+  supervisor?: string
+}
+
+interface BambooAPIResponse {
+  fields: {id: string; type: string; name: string}[]
+  employees: BambooEmployee[]
+}
+
+// the BambooEmployee type comes from the API call, but the Employee type
+// is the normalized type that we use in the app
+interface Employee extends Omit<BambooEmployee, 'id'> {
+  name: string
+  id: ID
+}
+
+interface Cache {
+  date?: number // Date.now()
+  data?: {
+    ids: {[id: string]: Partial<Employee>}
+    employees: Partial<Employee>[]
+    byName: {[name: string]: ID}
+    byTeam: {[team: string]: ID[]}
+    byDirectReports: {[name: string]: ID[]}
+    byTransitiveReports: {[name: string]: ID[]}
+    teamNames: string[]
+    managerNames: string[]
+    managerIds: ID[]
+  }
+}
+
+const cache: Cache = {}
+
+const getName = (employee: BambooEmployee): string  => employee.displayName + (employee.preferredName ? ` (${employee.preferredName})` : '')
+
+// takes an aggregation returned by a _.groupBy call and turns the values into a list of IDs rather than objects
+function byIds<T extends {[name: string]: BambooEmployee[]}>(
+  aggregation: T,
+): {[name in keyof T]: ID[]} {
+  return _.mapValues(aggregation, arr => arr.map(val => val.id.toString()))
+}
+
+function byFirstId<T extends {[name: string]: BambooEmployee[]}>(aggregation: T) {
+  const byIdArr = byIds(aggregation)
+  return _.mapValues(byIdArr, arr => arr[0]) 
+}
+
+function withOnlyTruthyValues<T extends {}>(obj: T): Record<string, NonNullable<T[keyof T]>>{
+  return _.omitBy(obj, val => !val)
+}
+
+export function getBambooData() {
+  if (cache.date && cache.date + CACHE_EXPIRATION_MS > Date.now()) return Promise.resolve(cache.data)
+
+  const authHeaderValue = `Basic ${base64.encode(`${BAMBOOHR_KEY}:x`)}`
+  const dataPromise = fetch(
+    `https://api.bamboohr.com/api/gateway.php/${BAMBOOHR_SUBDOMAIN}/v1/employees/directory`,
+    {headers: {accept: 'application/json', authorization: authHeaderValue}},
+  )
+    .then(res => res.json())
+    .then((response: BambooAPIResponse) => {
+      cache.date = Date.now()
+
+      // any normalization
+      const employees: Employee[] = response.employees.map(e => ({...e, id: e.id.toString(), name: getName(e)}))
+
+      // maps ids to the employee object
+      const ids = withOnlyTruthyValues(_.mapValues(_.groupBy(employees, e => e.id), arr => arr[0]))
+      if (_.size(employees) !== _.size(ids)) console.warn('WARNING: duplicate BambooHR IDs found')
+
+      // the data.by<Representation> keys map some key to a list of IDs
+      // TODO: make this work in the case where more than one person has the same displayName
+      const byName: {[name: string]: ID} = withOnlyTruthyValues(byFirstId(_.groupBy(employees, e => e.displayName)))
+      if (_.size(employees) !== _.size(byName)) console.warn('WARNING: duplicate BambooHR displayNames found')
+      const byTeam: {[team: string]: ID[]} = byIds(_.groupBy(employees, e => e.department))
+      const byDirectReportsIncludingCEO: {[name: string]: ID[]} = byIds(_.groupBy(employees, e => e.supervisor))
+      // CEO's supervisor can be falsy, so we should handle that
+      const byDirectReports = _.omitBy(byDirectReportsIncludingCEO, (_, key) => !key)
+      const managerCountDelta = _.size(byDirectReportsIncludingCEO) - _.size(byDirectReports)
+      if (managerCountDelta == 1) {
+        console.debug("DEBUG: there was a manager with a falsy name. the app assumes that it was caused by the CEO reporting to ''")
+      }
+      if (managerCountDelta > 1) console.warn('WARNING: multiple there are multiple managers with falsy names')
+
+      function getEmployeeAndTransitiveReports(id: string): string[] {
+        const name = ids[id]?.displayName
+        if (!name || !byDirectReports[name]) return [id]
+        return [id, ...(_.flatMap(byDirectReports[name], getEmployeeAndTransitiveReports))]
+      }
+      const byTransitiveReports: {[name: string]: string[]}  = _.mapValues(byDirectReports, ids => _.flatMap(ids, getEmployeeAndTransitiveReports))
+
+      // the data.<Object>s keys list the object keys
+      const teamNames = Object.keys(byTeam)
+      const managerNames = Object.keys(byDirectReports)
+      const managerIds = managerNames.map(name => byName[name]).filter(_.isString)
+
+      const data = {
+        // ensure we only keep non-sensitive info
+        ids: _.mapValues(ids, e => _.pick(e, ALLOWED_KEYS)),
+        employees: employees.map(e => _.pick(e, ALLOWED_KEYS)),
+
+        byName,
+        byTeam,
+        byDirectReports,
+        byTransitiveReports,
+        teamNames,
+        managerNames,
+        managerIds,
+      }
+      cache.data = data
+      console.log(`[${new Date().toLocaleString('en-US')}] BambooHR cache warmed with ${data.employees.length} employees`)
+      return data
+    })
+
+  return cache.data ? Promise.resolve(cache.data) : dataPromise
+}
