7c6daf4978eb838922794b9718e860146805c90b
Jordan Hayashi <jhhayashi@gmail.com>
2021-03-21 15:46:16 -0400

    Add nextjs setup

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  distDir: 'dist',
+  poweredByHeader: false,
+  reactStrictMode: true,
+}
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,20 @@
 import {AddressInfo} from 'net'
+import next from 'next'
 
 import app from './app'
 
-const PORT = process.env["port"] || 8080
+const PORT = process.env["PORT"] || 8080
+const DEV = process.env["NODE_ENV"] != 'production'
 
-const server = app.listen(PORT, () => {
-  const { port } = server?.address() as AddressInfo
-  console.log(`Server listening at http://localhost:${port}`)
+if (DEV) console.log('Server running in development mode')
+
+const nextServer = next({dev: DEV})
+const nextHandler = nextServer.getRequestHandler()
+
+nextServer.prepare().then(() => {
+  app.use((req, res) => nextHandler(req, res))
+  const expressServer = app.listen(PORT, () => {
+    const { port } = expressServer?.address() as AddressInfo
+    console.log(`Server listening at http://localhost:${port}`)
+  })
 })
