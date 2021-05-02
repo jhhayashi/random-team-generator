2deac8876082c651e07f358476063fd3966b2192
Jordan Hayashi <jhhayashi@gmail.com>
2021-05-02 00:02:52 -0400

    Add functions to get all slack users

--- server/slack/data.ts
+++ server/slack/data.ts
@@ -59,6 +59,16 @@ interface UsersProfileGetResult extends WebAPICallResult {
   }
 }
 
+interface UsersListResult extends WebAPICallResult {
+  members: {
+    id: string
+    deleted: boolean
+    profile: UsersProfileGetResult['profile']
+    is_bot: boolean
+    is_email_confirmed: boolean
+  }[]
+}
+
 export interface Cache {
   channelsList?: {
     date: number // Date.now()
@@ -69,7 +79,12 @@ export interface Cache {
     date: number
     members: ID[]
   }>
-  users?: Record<string, {date: number, profile: SlackUser}>
+  users?: {
+    // this date reflects how recently we've fetched all users. we'll also track
+    // when each profile was last fetched.
+    date: number
+    profiles: Record<string, {date: number, profile: SlackUser}>
+  }
 }
 
 export const cache: Cache = {channelsMembers: {}}
@@ -115,26 +130,60 @@ function fetchAllMembersInChannel(channelId: string): Promise<ConversationsMembe
   )
 }
 
-function groupChannelsByKey(channels: Channel[], key: string) {
-  return _.mapValues(_.groupBy(channels, key) as Record<string, [typeof channels[number]]>, arr => arr[0])
+function isValidUser(profile: UsersListResult['members'][number]): boolean {
+  return !profile.is_bot && !profile.deleted && profile.is_email_confirmed
+}
+
+function fetchAllUsers(): Promise<SlackUser[]> {
+  return fetchAllPaginatedData(
+    cursor => webClient.users.list({cursor, limit: 200}) as Promise<UsersListResult>,
+    (res: UsersListResult) => res.members.filter(isValidUser).map(member => convertSlackProfileToUser(member.profile, member.id)),
+  )
+}
+
+function groupListByKey<T>(arr: T[], key: string): Record<string, T> {
+  return _.mapValues(_.groupBy(arr, key) as Record<string, [T]>, arr => arr[0])
+}
+
+function convertSlackProfileToUser(user: UsersProfileGetResult['profile'], id: ID): SlackUser {
+  const {display_name_normalized, display_name, image_512, real_name_normalized, real_name} = user
+  return {
+    id,
+    name: real_name_normalized || display_name_normalized || real_name || display_name || '',
+    imgUrl: image_512,
+  }
 }
 
 function getUserById(userId: string): Promise<SlackUser> {
-  const cachedUser = cache?.users?.[userId]
+  const cachedUser = cache?.users?.profiles?.[userId]
   const cachedDate = cachedUser && cachedUser.date
   if (cachedUser && cachedDate && cachedDate + CACHE_EXPIRATION_MS > Date.now()) return Promise.resolve(cachedUser.profile)
 
   const apiPromise = webClient.users.profile.get({user: userId}) as Promise<UsersProfileGetResult>
   return apiPromise.then((response: UsersProfileGetResult) => {
     const {profile} = response
-    const user: SlackUser = {
-      id: userId,
-      name: profile.real_name_normalized || profile.display_name_normalized || profile.real_name || profile.display_name || '',
-      imgUrl: profile.image_512,
-    }
+    const user = convertSlackProfileToUser(profile, userId)
     _.set(cache, `users.${userId}`, {date: Date.now(), profile: user})
     return user
   })
+}
+
+export function getAllUsers() {
+  if (!ENABLED) return Promise.reject(new IntegrationDisabledError('Slack integration is disabled'))
+
+  const cacheDate = cache?.users?.date
+  if (cacheDate && cacheDate + CACHE_EXPIRATION_MS > Date.now()) return Promise.resolve(_.map(cache?.users?.profiles, 'profile'))
+  const startTime = Date.now()
+  return fetchAllUsers()
+    .then(users => {
+      const profiles = groupListByKey(users, 'id')
+      const profilesWithCacheDate = _.mapValues(profiles, profile => ({date: startTime, profile}))
+      cache.users = {date: startTime, profiles: profilesWithCacheDate}
+      log(`Slack channels cache warmed with ${users.length} users in ${Date.now() - startTime}ms`)
+      console.log(users)
+      console.log(cache)
+      return users
+    })
 }
 
 export function getSlackChannels(): Promise<Cache['channelsList']> {
@@ -148,8 +197,8 @@ export function getSlackChannels(): Promise<Cache['channelsList']> {
       const slimmedChannels: Channel[]  = channels.map(({id, name, num_members}) => ({id, name, memberCount: num_members}))
       const channelsList = {
         date: startTime,
-        channelsById: groupChannelsByKey(slimmedChannels, 'id'),
-        channelsByName: _.mapValues(groupChannelsByKey(slimmedChannels, 'name'), channel => channel.id),
+        channelsById: groupListByKey(slimmedChannels, 'id'),
+        channelsByName: _.mapValues(groupListByKey(slimmedChannels, 'name'), channel => channel.id),
       }
 
       cache.channelsList = channelsList
